5240c8ccfb4868f45727093ea8c8821774495338
Никита Столетов <Tyuiu.StoletovNA@outlook.com>
2024-12-12 20:32:55 +0500

    Task3

---
+++ Tyuiu.StoletovNA.Sprint5.Task3.V9.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.StoletovNA.Sprint5.Task3.V9.Lib {
+    public class DataService : ISprint5Task3V9 {
+        public string SaveToFileTextData(int x)
+        {
+            string fullPath = Path.Combine(new string[] { Path.GetTempPath(), "OutPutFileTask3.txt" });
+
+            
+
+            double y = Math.Pow(x, 3) / (Math.Pow(x, 2) - 1);
+            y = Math.Round(y, 3);
+
+            using (BinaryWriter writer = new BinaryWriter(File.Open(fullPath, FileMode.OpenOrCreate), Encoding.UTF8))
+            {
+                writer.Write(BitConverter.GetBytes(y));
+            }
+
+            return fullPath;
+        }
+    }
+}
---
+++ Tyuiu.StoletovNA.Sprint5.Task3.V9/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using Tyuiu.StoletovNA.Sprint5.Task3.V9.Lib;
+namespace Tyuiu.StoletovNA.Sprint5.Task3.V9 {
+    class Program {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #5 | Выполнил: Столетов Никита Андреевич | ИИПБ-24-2";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Класс File. Запись набора данных в текстовый файл                       *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #9                                                             *");
+            Console.WriteLine("* Выполнил: Столетов Никита Андреевич | ИИПБ-24-2                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+
+            Console.WriteLine("*Дано выражение вычислить его значение при x = 3                          *");
+
+            Console.WriteLine("*заполненный значениями с клавиатуры                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+
+
+            int x = 3;
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.SaveToFileTextData(x);
+            Console.WriteLine("Файл: " + result);
+
+            
+
+            Console.ReadKey();
+        }
+    }
+}
