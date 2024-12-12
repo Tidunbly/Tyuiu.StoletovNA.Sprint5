3ea4c4b1db893f0a6c60d36025f95c42059e1e43
Никита Столетов <Tyuiu.StoletovNA@outlook.com>
2024-12-13 00:39:17 +0500

    Task6

---
+++ Tyuiu.StoletovNA.Sprint5.Task6.V1.Lib/DataService.cs
@@ -0,0 +1,16 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using tyuiu.cources.programming.interfaces.Sprint5;
+
+namespace Tyuiu.StoletovNA.Sprint5.Task6.V1.Lib {
+    public class DataService : ISprint5Task6V1 {
+        public int LoadFromDataFile(string path)
+        {
+            string str = File.ReadAllText(path);
+
+            int digitCount = str.Count(char.IsDigit);
+            return digitCount;
+
+        }
+    }
+}
---
+++ Tyuiu.StoletovNA.Sprint5.Task6.V1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.IO;
+using Tyuiu.StoletovNA.Sprint5.Task6.V1.Lib;
+namespace Tyuiu.StoletovNA.Sprint5.Task6.V1 {
+    class Program {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #5 | Выполнил: Столетов Никита Андреевич | ИИПБ-24-2";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Класс File. Запись набора данных в текстовый файл                       *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнил: Столетов Никита Андреевич | ИИПБ-24-2                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+
+            Console.WriteLine("*Дан файл С:\\DataSprint5\\InPutDataFileTask6V1.txt                       *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string path = @"C:\DataSprint5\InPutDataFileTask6V1.txt";
+            
+
+
+
+
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.LoadFromDataFile(path);
+            Console.WriteLine(result);
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
